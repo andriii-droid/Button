aa224d352f1304446df6a6b0bce5f870a670cc53
andriii-droid <andri.andermatt@icloud.com>
2025-04-27 16:41:32 +0200

    Resturcured Button.h file anddeleted Button.cpp and main.cpp

--- include/Button.h
+++ include/Button.h
@@ -8,79 +8,35 @@
     class Button
     {
         public:
-            enum edgeType {negative, positive, toggle, timePos, timeNeg, doubleTap}; 
-            //enum for diffrent Trigger types 
+            enum outputState {none, time, twice, click, error};
 
-            explicit Button(int initPin, edgeType initEdge); 
+            explicit Button(int initPin); 
             //Inititialises the Button, 
             //pin: Pin Number of Button
-            //trigger: Defines behaovior of state. 
-            //Possible Values: nature negative, positive, toggle, time, double
-            //initTrigTime: Time if edgeType == timeTap; default is 2000
-      
-            bool getState() const { return state;}
+           
+            outputState getState() const {return state;}
             //Gets current State of the Button
-
-            bool getNature() const {return butNature(); }
-            //Gets nature State of the Button
-
-            edgeType getTrigger() const {return edge;}
-            //Gets the Trigger Type of the Button
 
             void updateButton();
             //Updates the State of the Button 
             //needs to be run once per Loop.
             //Preferably at the Start of every Loop
-            //After all updateButton() Functions it is Recommended to put a delay(5) to avoid bouncing
 
         private:
-            bool state = LOW;
-            bool stateNeg = LOW; //State of Button
-            bool statePos = LOW; //State of Button
-            long timeLestPress = 0;  //Timestamp of Last Press
+            int pin; //Pin Number
 
-            struct statesStruct
-            {
-                bool negative = 0;
-                bool positive = 0;
-                bool timePos = 0;
-                bool timeNeg = 0;
-                bool toggle = 0;
-                bool doubleTap = 0;
-            };
+            outputState state = none; //State of the Button
+            int logTime = 1000; //Time the Button has to log Edges in ms
+            int posEdges = 0; //Number of positive Edges during Log Time
+            int negEdges = 0; //Number of negative Edges during Log Time
 
-            statesStruct outState;
+            bool startLog();
+            //Returns true if Button gets pressed
 
-            edgeType edge; //Defines how state is set
-            int pin; //Pin of the Button
+            bool getPosEdge();
+            //Returns true, if positive Edge is detected
 
-            bool trigLastPos = LOW; //Helper for Triggers
-
-            bool trigLastNeg = HIGH; //Helper for Triggers
-
-            bool trigToggle = LOW; //Helper for Toggle
-
-            long trigTime;  //Time that Button needs to be pressed
-            long timeLastEdge = 0;  //Timestamp of Last Edge
-            bool trigLastTime = LOW; //Helper for Time Trigger
-            bool trigEdgeTime = LOW; //Helper for Time Trigger
-
-            int counter = 0; //Counter for double Tap
-            int doubleDelay = 0;
-
-            bool butNature() const {return digitalRead(pin);}
-            //Gets Button State Nature
-
-            bool getButtonPos();
-
-            bool getButtonNeg();
-
-
-            bool getDouble();
-            //Gets Double Tap state
-
-
-
-
+            bool getNegEdge();
+            //Returns true, if negative Edge is detected
     };
 #endif //BUTTON_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <Arduino.h>
 #include <Button.h>
 
-
-
-Button b1 {15, b1.positive};
-Button b2 {15, b2.positive};
-
 void setup() {
   Serial.begin(9600);
   Serial.println("Setup Complete");
@@ -22,16 +17,5 @@ void setup() {
 }
 
 void loop() {
-  b1.updateButton();
-  // b2.updateButton();
-  // delay(5);
 
-  // digitalWrite(16, b1.getState());
-  // digitalWrite(17, b2.getState());
-
-  // if (b1.getState())
-  // {
-  //   Serial.println("TrigPos");
-  // }
-  
 }
